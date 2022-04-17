38a472361535f63c0b3a5ac3be8671aecd92fc43
nbljain <93477193+nbljain@users.noreply.github.com>
2022-04-17 14:24:59 +0530

    Add files via upload

---
+++ review.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sid = SentimentIntensityAnalyzer()
+
+st.write("""
+# Inaccrate Rating Prediction App
+This app predicts the **Inaccurate** Ratings!
+""")
+
+st.sidebar.header('User Input Features')
+
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        ID = st.sidebar.selectbox('ID', list((range(3886,684991))))
+        Star = st.sidebar.selectbox('Star',list(range(1,5)))
+        data = {'ID':ID,
+               'Star':Star
+                }             
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+st.write(input_df)
+st.subheader('Prediction')
+cleaned_data= pd.read_csv('cleaned_reviews.csv')
+st.write(cleaned_data)
