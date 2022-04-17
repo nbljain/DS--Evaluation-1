19e8acf1167938d3aba50a06c69cdad503821cee
nbljain <93477193+nbljain@users.noreply.github.com>
2022-04-17 14:43:47 +0530

    Update review.py

--- review.py
+++ review.py
@@ -4,14 +4,14 @@ import numpy as np
 
 
 st.write("""
-# InaccUrate Rating Prediction App
+# Inaccurate Rating Prediction App
 This app predicts the **Inaccurate** Ratings!
 """)
 
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+[Example CSV input file](https://raw.githubusercontent.com/example.csv)
 """)
 
 # Collects user input features into dataframe
